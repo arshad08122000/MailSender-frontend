12df798cbcc965763442069389f8ec9a530d1eaa
Arshad .I <iarshad2000@gmail.com>
2023-03-04 18:38:15 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
 import logo from './logo.svg';
-import './App.css';
+import './Home.css';
+import {useState} from 'react';
+import axios from 'axios';
 
 function App() {
+
+ const [email,setEmail]=useState("");
+ const [description,setDescription]=useState("");
+
+
+
+ const handleSubmit=async(e)=>{
+   e.preventDefault();
+   const data={email,description};
+
+   console.log("Email :",email);
+    console.log("Description :",description);
+
+
+   const response=await axios.post("http://localhost:8000/mail",data);
+   console.log(response);
+   alert("Mail Sended");
+   window.location.reload(false);
+ }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="signup-form">
+      <form onSubmit={handleSubmit}>
+     <h2 className="mb-5">React Mail Sender</h2>
+     <hr />
+      <div class="form-group">
+       <input type="email" class="form-control" onChange={(e)=>setEmail(e.target.value)} name="email" placeholder="Email" required="required">
+      </input></div>
+        <div class="form-group">
+          <input type="text" class="form-control" onChange={(e) => setDescription(e.target.value)} name="description" placeholder="Description" required="required">
+          </input></div>
+      <div class="form-group">
+       <button type="submit" class="btn btn-primary">Send</button>
+      </div> 
+      </form>
     </div>
   );
 }
